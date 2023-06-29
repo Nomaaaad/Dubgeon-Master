35ff95b2f409b91cbff3d9b3e14a4d739cbe6251
Javid Huseynov <pfvpfrf11@gmail.com>
2023-06-29 15:58:00 +0400

    Impoting assets and Creating scriptable objects

--- Assets/Scripts/Dungeon/Doorway.cs
+++ Assets/Scripts/Dungeon/Doorway.cs
@@ -1,22 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 [System.Serializable]
-
-
 public class Doorway 
 {
     public Vector2Int position;
     public Orientation orientation;
     public GameObject doorPrefab;
-
-    #region
+    #region Header
     [Header("The Upper Left Position To Start Copying From")]
     #endregion
     public Vector2Int doorwayStartCopyPosition;
-    #region
+    #region Header
     [Header("The width of tiles in the doorway to copy over")]
     #endregion
     public int doorwayCopyTileWidth;
-    #region
+    #region Header
     [Header("The height of tiles in the doorway to copy over")]
     #endregion
     public int doorwayCopyTileHeight;
